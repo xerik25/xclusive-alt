a8675cede46415fe22994fa36395af0ec70000df
natedean <nate.dean@vml.com>
2015-08-16 14:28:44 -0500

    Removed all console errors. Had to comment out some $.subscribe and $.publish methods that were blowing up, but it does not appear that the app is using them, it is possible that much of the Helpers.js could be removed completely.

--- htdocs/src/javascript/helpers/Helpers.checkWidth.js
+++ htdocs/src/javascript/helpers/Helpers.checkWidth.js
@@ -1,16 +1,16 @@
 App.Helpers.checkWidth = (function(conds) {
 
 	conds.init = function() {
-		$.subscribe('resize', function(){
-			// console.log('resize event fired');
-			App.Helpers.getWidth();
-
-			// if mobile width, mobile media query or mobile ua returns true, broadcast a resize:mobile event
-			if ( App.Devices.mobile || App.Devices.mobileMQ || App.Devices.mobileCheck ) $.publish('resize:mobile');
-
-			// if we are in the tablet realm, lets publish that
-      if ( App.Devices.tablet || App.Devices.tabletMQ ) $.publish('resize:tablet');
-		});
+		//$.subscribe('resize', function(){
+		//	// console.log('resize event fired');
+		//	App.Helpers.getWidth();
+    //
+		//	// if mobile width, mobile media query or mobile ua returns true, broadcast a resize:mobile event
+		//	if ( App.Devices.mobile || App.Devices.mobileMQ || App.Devices.mobileCheck ) $.publish('resize:mobile');
+    //
+		//	// if we are in the tablet realm, lets publish that
+     // if ( App.Devices.tablet || App.Devices.tabletMQ ) $.publish('resize:tablet');
+		//});
 	};
 
 	return conds;
