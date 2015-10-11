234afd398b0c41edb04753abb6aa8b50c740b9b9
Nate Dean <natedean@Nates-MacBook-Pro.local>
2015-10-11 14:50:56 -0500

    Modified app.js to be able to accept PORT number as first arg, also switched to some ES2015 types, arrow functions, and template strings (for fun)

--- htdocs/app.js
+++ htdocs/app.js
@@ -1,12 +1,16 @@
-var app = require('koa')();
-var serve = require('koa-static');
-var cors = require('koa-cors');
-var router = require('./server/routes');
+'use strict';
+
+let app = require('koa')();
+const serve = require('koa-static');
+const cors = require('koa-cors');
+const router = require('./server/routes');
+
+const PORT = process.argv[2] || 3001;
 
 app.use(cors());
 app.use(router.routes());
 app.use(serve('./app'));
 
-console.log('Koa server listening on port 3000');
-
-app.listen(3000);
+app.listen(PORT, ()=>{
+  console.log(`Koa server listening at PORT ${PORT}`);
+});
